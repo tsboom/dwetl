d9c8ea5b8b0e6c49df836da82a53ffc71bbb0fd3
Tiffany Schoneboom <thschone@LIBRLTSM237195.fios-router.home>
2019-04-02 00:58:36 -0400

    LIBILS-266 created transformfield class

---
+++ TransformField.py
@@ -0,0 +1,26 @@
+'''
+Transform Field
+
+A class to represent a field to be transformed
+'''
+
+class TransformField:
+
+    def __init__(self, name, value, isbn_issn_code = None):
+        self.name = name
+        self.value = value
+        self.isbn_issn_code = isbn_issn_code
+        self.log = {
+            "pp": None,
+            "dq" : None,
+            "transforms" : []
+        }
+
+    def log_transform_result(self, transform_name, result):
+        self.log["transforms"].append({name: transform_name, result: result})
+
+    def log_pp(self, result):
+        self.log['pp'] = result
+
+    def log_dq(self, result):
+        self.log['dq'] = result
