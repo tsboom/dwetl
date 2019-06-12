9e4bef6cbf5868bc05974f6b9611b99be7e389ca
Joseph Koivisto <jkoivist@umd.edu>
2019-06-12 13:04:13 -0400

    Minor updates to data_quality_utlities; no substantial change to dw_etl; bibliographic_record_dimension and library_item_dimensions updated to change function name to no_missing_values; table_transform has bug checking terminal outputs and traces for personal review; test_table_transform updated with new unittests

--- table_transform.py
+++ table_transform.py
@@ -15,7 +15,7 @@ from sqlalchemy import MetaData
 from TransformField import TransformField
 import data_quality_specific_functions as dqs
 import data_quality_utilities as dqu
-# import specific_transform_functions
+import specific_transform_functions
 
 
 
@@ -29,19 +29,15 @@ def load_table_config(table_config_path):
 def get_isbn_issn_code(column, row_dict):
     isbn_issn_column = 'in_z13_isbn_issn_code'
     optional_isbn_code = None
-    if column == isbn_issn_column:
-        # get ISBN code from first three chars, save code to var
-        isbn_issn_code = row_dict[isbn_issn_column][0:2]
-    # column name contains ISBN_ISSN, optional_isbn_code = isbn_code
     if column == 'in_z13_isbn_issn':
         # get ISBN code from first three chars, save code to var
-        isbn_issn_code = row_dict[isbn_issn_column][0:2]
-        optional_isbn_code = isbn_issn_code
+        optional_isbn_code = row_dict[isbn_issn_column][0:3]
         return optional_isbn_code
     return optional_isbn_code
-
-
+#^^ the first if seems like it will always return NONE, correct?
+#^^ updated to return substring 0:3 because it does say you need the first three characters, yes?
 # creates array of TransformField objects which takes into account ISBN/ISSN code
+
 def transform_row(sa_row):
     # fields to transform
     fields = []
@@ -52,7 +48,7 @@ def transform_row(sa_row):
             optional_isbn_code = get_isbn_issn_code(column, row_dict)
             fields.append(TransformField(column, field_value, isbn_issn_code=optional_isbn_code))
     return fields
-
+    
 
 # Write field transform log to stage 2 db
 def write_log(field_name, log):
@@ -69,7 +65,6 @@ def preprocess(field, source_col_sorted_dict):
         print('No preprocessing for ' + field.name)
         return field.value
 
-
 def functions_from_module(module):
     functions = inspect.getmembers(module, inspect.isfunction)
     return functions
@@ -79,6 +74,12 @@ def functions_from_module(module):
 def execute_dq_function(function_name, arg, input, dq_funcs_list):
     is_passing = ''
     # search for function in dq list, execute with params
+    print(function_name)
+    print(arg)
+    print(str(arg))
+    print(len(arg))
+    print(input)
+    pdb.set_trace()
     for function in dq_funcs_list:
         function_object = function[1]
         dq_function = function[0]
@@ -103,6 +104,7 @@ def check_data_quality(field, dq_funcs_list, obj):
             function_name = check.get('specific_dq_function')
             arg = check.get('specific_dq_function_param_1')
             is_passing = execute_dq_function(function_name, arg, field.value, dq_funcs_list)
+            print("IS PASSING VARIABLE IS" + is_passing)
             if is_passing:
                 field_dq_result = field.value
             else:
@@ -145,14 +147,12 @@ def transform_field(field, source_col_sorted_dict):
     '''
     Using the field name and value, run transformations and log to the field's log
     '''
-
+#^^do we think it is wise to have a function with the same name as a class and additional python file? Or does it not matter?
     # run pp
     result = preprocess(field, source_col_sorted_dict)
     field.record_pp(result)
-
     # set up dq
     dq_funcs_list = functions_from_module(dqs) + functions_from_module(dqu)
-
     # run dq
     run_dq_checks(field, dq_funcs_list, source_col_sorted_dict)
 
@@ -180,6 +180,7 @@ def transform_stg2_table(engine, source_col_sorted_dict, table, dwetl_logger):
     session = Session()
 
     for row in session.query(table).all():
+        print(row)
         row_fields = transform_row(row)
         for field in row_fields:
             transform_field(field, source_col_sorted_dict)
--- tests/test_table_transform.py
+++ tests/test_table_transform.py
@@ -1,52 +1,98 @@
 #import dw_etl
 import os
 import pdb
-<<<<<<< HEAD
-import table_transform
 import unittest
-
-
-
-=======
-import unittest
-
 from table_transform import *
->>>>>>> 2b1064dc57f3e8aaf4e135fba6c9166e159e637e
-
+from .data.test_variables import * 
+from TransformField import *
 
 '''
 Unit tests for table_transform.py
 '''
-<<<<<<< HEAD
-VALID_TEST_TSV_PATH = os.path.join('tests','data','mai50_z305_20181115_172016_1.tsv')
 
-
-# check to see if tsv can be imported into not empty dataframe
-#def test_load_table_config():
-#    assert table_config['title'] == 'z30'
 class TestTableTransform(unittest.TestCase):
+    
     def test_load_table_config(self):
         TABLE_PATH = os.path.join('tests','data','test_table_config_z30.json')
-        table_config = table_transform.load_table_config(TABLE_PATH)
+        table_config = load_table_config(TABLE_PATH)
         self.assertEqual(table_config['title'],'z30')
-=======
+        
+    def test_get_isbn_issn_code(self):
+        self.assertEqual(get_isbn_issn_code('in_z13_isbn_issn',data_get_isbn_issn_none),'   ')
 
-class TestTableTransform(unittest.TestCase):
+        self.assertEqual(get_isbn_issn_code('in_z13_isbn_issn',data_get_isbn_issn_020),'020')
 
-    def test_check_data_quality():
->>>>>>> 2b1064dc57f3e8aaf4e135fba6c9166e159e637e
+        self.assertEqual(get_isbn_issn_code('in_z13_isbn_issn',data_get_isbn_issn_022),'022')
+
+        self.assertEqual(get_isbn_issn_code('in_z13_isbn_issn',data_get_isbn_issn_0220),'022')
+
+    def test_preprocess(self):
+        #testing trim preprocessing function
+        a = TransformField('in_z13_title','   A title with some extraneous spaces     ')
+        self.assertEqual(preprocess(a,data_source_col_sorted),'A title with some extraneous spaces')
+
+        #testing date no preprocessing no output $$$this is a bad test as processing occurs after dq whoops
+        b = TransformField('in_z13_open_date',20021124)
+        self.assertEqual(preprocess(b,data_source_col_sorted),20021124)
+
+        #testing terminal output of no preprocessing
+        #c = TransformField('in_z13_upd_time_stamp',201708251637466)
+        #with self.assertRaises(KeyError):
+        #    preprocess(c,data_source_col_sorted)
+            #doesn't seem to be working with the KeyError?
+
+        #z13u preprocess test
+
+#    def test_dq_func_list(self):
+        
+    def test_func_from_mod(self):
+        dqu_check = functions_from_module(dqu)
+        dqs_check = functions_from_module(dqs)
+
+        #test output is list
+        self.assertIsInstance(dqu_check, list)
+        self.assertIsInstance(dqs_check, list)
+
+        '''test sys generated list of function name matches established test variable, if new checks are added, this test will fail, so a nice check on future wor;
+        I'm honestly not sure how to work with the regularly returned output of functions_from_module '''       
+        func_names = []
+        for i in dqu_check:
+            func_names.append(i[0])
+        for i in func_names:
+            self.assertTrue(i in dqu_func_names_list)
+
+        spec_func_names = []
+        for i in dqs_check:
+            spec_func_names.append(i[0])
+        for i in spec_func_names:
+            self.assertTrue(i in dqs_func_names_list)
+
+#    def test_run_dq_checks(self):
+
+#    def test_check_dq(self):
+
+    def test_exec_dq_func(self):
+        
+        dq_funcs_list = functions_from_module(dqs) + functions_from_module(dqu)
+
+        #test no_missing_values
+        self.assertTrue(execute_dq_function('no_missing_values','',20021124,dq_funcs_list))    
+        self.assertFalse(execute_dq_function('no_missing_values','',0,dq_funcs_list))    
+
+        #test dq_z13_user_defined_2
+        self.assertTrue(execute_dq_function('dq_z13u_user_defined_2','','ocm00024372',dq_funcs_list))
+        self.assertFalse(execute_dq_function('dq_z13u_user_defined_2','','ocm333',dq_funcs_list))
+        self.assertTrue(execute_dq_function('dq_z13u_user_defined_2','','ocn464584694',dq_funcs_list))
+        self.assertTrue(execute_dq_function('dq_z13u_user_defined_2','','on1245789453',dq_funcs_list))
+        
+#    def test_check_data_quality():
 
         # when there's no dq check, field.value is used
 
-<<<<<<< HEAD
-#VALID_TEST_TSV_PATH = os.path.join('tests','data','test_mai50_z305_20181115_172016_1.tsv')
-#df = dwetl.read_tsv_into_dataframe(VALID_TEST_TSV_PATH)
-=======
         # each dq check is called with its parameters provided
->>>>>>> 2b1064dc57f3e8aaf4e135fba6c9166e159e637e
 
         # field.value is used whenever a dq check is passing
-        self.assertTrue(data_quality_utilities.is_valid_length(5634563, 7))
+#        self.assertTrue(data_quality_utilities.is_valid_length(5634563, 7))
 
         # replacement_value is used whenever dq check is failing
 
@@ -62,7 +108,6 @@ if __name__ == '__main__':
     unittest.main()
 ####
 '''
-
 input type -> target type change
 field name change
 data quality checks executed
