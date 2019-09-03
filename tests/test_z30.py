115e24a887432e0e412c688580781d88f3100b6d
Tiffany Schoneboom <thschone@umd.edu>
2019-09-03 15:21:38 -0400

    update z30_rec_key tests

--- tests/test_z30.py
+++ tests/test_z30.py
@@ -7,21 +7,42 @@ from TransformField import *
 
 class Test_z30_rec_key(unittest.TestCase):
 
-    def setUp(self):
-        # load table config JSON
-        table_config_path = os.path.join('table_config', 'library_item_dimension.json')
-        table_config = table_transform.load_table_config(table_config_path)
+    # def setUp(self):
+
 
 
     def test_valid_z30_rec_key(self):
+        # setUp
+        # load table config JSON
+        table_config_path = os.path.join('table_config', 'library_item_dimension.json')
+        table_config = load_table_config(table_config_path)
+
         # create dict
-        z30_rec_key = {'in_z30_rec_key': '000001200000020'}
+        # z30_rec_key = {'in_z30_rec_key': '000001200000020'}
+        z30_rec_key = TransformField('in_z30_rec_key', '000001200000020')
 
         # feed into something
-        result = table_transform.transform_field(z30_rec_key, table_config)
-        self.assertEqual('000001200000020', result['t1_z30_rec_key__lbry_item_source_system_id'])
-        # self.assertEqual('000001200000020', result['pp_z30_rec_key'])
-        # self.assertEqual('000001200000020', result['dq_z30_rec_key'])
+        transform_field(z30_rec_key, table_config)
+
+        # test pp
+        self.assertEqual('000001200000020', z30_rec_key.record['pp'])
+        # test dq
+        expected_dqs = [
+            {'name': 'no_missing_values', 'result': '000001200000020', 'target_col_name': 'LBRY_ITEM_SOURCE_SYSTEM_ID'},
+            {'name': 'is_valid_length', 'result': '000001200000020', 'target_col_name': 'LBRY_ITEM_SOURCE_SYSTEM_ID'},
+             {'name': 'is_numeric', 'result': '000001200000020', 'target_col_name': 'LBRY_ITEM_SOURCE_SYSTEM_ID'}
+        ]
+        self.assertEqual(expected_dqs, z30_rec_key.record['dq'])
+
+
+        # test transforms
+        expected_transforms = [
+            None,
+            {'name': 'substring','result': '000001200','target_col_name': 'lbry_item_adm_no'},
+            {'name': 'substring', 'result': '000020', 'target_col_name': 'lbry_item_seq_no'}
+        ]
+        self.assertEqual(expected_transforms, z30_rec_key.record['transforms'])
+
 
     # do replacement value checks later
     # def test_missing_z30_rec_key(self):
