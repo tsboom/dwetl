2994462e43fef688a97106c6b7b94b80903cd91f
David P. Steelman <dsteelma@umd.edu>
2019-09-13 08:23:23 -0400

    LIBILS-384. Updated ".env" to have test database settings
    Added additional settings to the "env_example" file for configuring
    a database to use for testing.
    
    Updated "database_credentials.py" to use the additional settings for
    configuring the test database.
    
    https://issues.umd.edu/browse/LIBILS-384

--- dwetl/database_credentials.py
+++ dwetl/database_credentials.py
@@ -3,18 +3,37 @@ from dotenv import load_dotenv
 load_dotenv()
 
 '''
-database credentials
-'''
+Application Database Settings
 
+Database settings for use when running the application.
+'''
 # The username to use in connecting to the database
 DB_USER = os.getenv("DB_USER")
 # The password to use in connecting to the database
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 # The name of the database
-DB_NAME = os.getenv("DB_NAME") or 'usmai_dw_etl'
+DB_NAME = os.getenv("DB_NAME")
 # The hostname/IP address of the Postgres server
-DB_HOST_NAME = os.getenv("DB_HOST_NAME") or '127.0.0.1'
+DB_HOST_NAME = os.getenv("DB_HOST_NAME")
 # The port to use to connect to the Postgres server
-DB_PORT= os.getenv("DB_PORT") or '5432'
+DB_PORT = os.getenv("DB_PORT")
 
 DB_CONNECTION_STRING = f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST_NAME}:{DB_PORT}/{DB_NAME}'
+
+'''
+Test Database Settings
+
+Database settings for use in conjunction with the tests.
+
+Note: In production, these settings should probably not be set.
+'''
+TEST_DB_USER = os.getenv("TEST_DB_USER")
+TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
+TEST_DB_NAME = os.getenv("TEST_DB_NAME")
+TEST_DB_HOST_NAME = os.getenv("TEST_DB_HOST_NAME")
+TEST_DB_PORT = os.getenv("TEST_DB_PORT")
+
+TEST_DB_CONNECTION_STRING = f'postgresql+psycopg2://{TEST_DB_USER}:{TEST_DB_PASSWORD}@{TEST_DB_NAME}:{TEST_DB_HOST_NAME}/{TEST_DB_PORT}'
+
+# Boolean used by tests requiring an actual database to determine whether the test database has been configured
+TEST_DB_CONFIGURED = TEST_DB_USER and TEST_DB_PASSWORD and TEST_DB_NAME and TEST_DB_HOST_NAME and TEST_DB_PORT
