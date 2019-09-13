9f50ef0e485b801eef27290addf1556c5f5015e1
David P. Steelman <dsteelma@umd.edu>
2019-09-13 15:46:33 -0400

    LIBILS-386. Added "PrintWriter" implementation
    A simple Reader that takes a List and outputs the items one at a time.
    
    https://issues.umd.edu/browse/LIBILS-386

---
+++ dwetl/writer/print_writer.py
@@ -0,0 +1,8 @@
+class PrintWriter:
+    """
+    Writer that sends each row to standard out.
+
+    This writer is mainly intended for tst.
+    """
+    def write_row(self, row):
+        print(row)
