d8154866598ac78e47576e56bd02e4a7fb2eb0b9
Tiffany Schoneboom <thschone@umd.edu>
2019-09-17 15:11:28 -0400

    LIBILS-388. Added class and factory class to build job_info objects.
    Created class to hold execution metadata.
    Decided to only do dw_prcsng_cycle table.
    
    https://issues.umd.edu/browse/LIBILS-388

--- dwetl/job_info.py
+++ dwetl/job_info.py
@@ -1,21 +1,52 @@
 import os
+import pdb
+import datetime
+import getpass
+from sqlalchemy import func
+import dwetl
+
 
 class JobInfo():
     """
-    This class finds out the processing metadata for a job.
-
-    It builds a job_info object.
+    This class holds the processing metadata for a job.
     """
-    def __init__(self, prcsng_cycle_id, job_exectn_id, job_version_no, user_id):
+    def __init__(self, prcsng_cycle_id, user_id, job_version_no, job_exectn_id):
         self.prcsng_cycle_id = prcsng_cycle_id
+        self.user_id = user_id
         self.job_exectn_id = job_exectn_id
         self.job_version_no = job_version_no
-        self.user_id = user_id
 
-    def set_prcsng_cycle_id(self, session):
-        '''
-        Look into the db to see if processing cycle id exists, and increment the number.
-        If there is nothing, use 1.
-        '''
-        # query to see what current process ID is
-        max_prcsng_id = self.session.query(func.max(table_base_class.em_create_dw_prcsng_cycle_id))
+
+class JobInfoFactory():
+    '''
+    builds job info objects
+    '''
+
+    @classmethod
+    def create_job_info_from_db(cls, session, table_base_class):
+        cls.session = session
+        cls.table_base_class = table_base_class
+        max_prcsng_id = cls.session.query(func.max(table_base_class.dw_prcsng_cycle_id)).scalar()
+        if max_prcsng_id == None:
+            cls.prcsng_cycle_id = 1
+        else:
+            cls.prcsng_cycle_id = max_prcsng_id + 1
+        cls.user_id = getpass.getuser()
+        cls.job_exectn_id = 1
+        cls.job_version_no = dwetl.version
+
+
+        row = {
+            'dw_prcsng_cycle_id': cls.prcsng_cycle_id,
+            'dw_prcsng_cycle_planned_dt': datetime.datetime.now(),
+            'dw_prcsng_cycle_stat_type_cd': '',
+            'dw_prcsng_cycle_freq_type_cd': '',
+            'dw_prcsng_cycle_exectn_start_tmstmp': datetime.datetime.now(),
+            'em_create_tmstmp': datetime.datetime.now(),
+            'em_create_user_id': cls.user_id
+        }
+
+        record = cls.table_base_class(**row)
+        cls.session.add(record)
+        cls.session.commit()
+        return JobInfo(cls.prcsng_cycle_id, cls.user_id, cls.job_version_no, cls.job_exectn_id)
