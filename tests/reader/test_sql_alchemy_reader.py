411f68d24474befec69b2448ad3828b061eb78e0
David P. Steelman <dsteelma@umd.edu>
2019-09-18 11:43:27 -0400

    LIBILS-390. Updated tests to use "test_database_session" context manager
    Updated tests to use "test_database_session" context manager instead
    of handling the database connection and session in the "setUp" and
    "tearDown" methods.
    
    For "test_sql_alchemy_reader.py" and "test_sql_alchemy_writer.py",
    left an empty "setUp" method, so that the unittest "skipUnless" could
    be added to one method, instead of each test method.
    
    https://issues.umd.edu/browse/LIBILS-390

--- tests/reader/test_sql_alchemy_reader.py
+++ tests/reader/test_sql_alchemy_reader.py
@@ -1,36 +1,19 @@
 import unittest
 from dwetl.reader.sql_alchemy_reader import SqlAlchemyReader
 import dwetl.database_credentials as database_credentials
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import create_engine
 import datetime
+import dwetl
 
 
 class TestSqlAlchemyReader(unittest.TestCase):
     @unittest.skipUnless(database_credentials.test_db_configured(), "Test database is not configured.")
     def setUp(self):
-        # See https://docs.sqlalchemy.org/en/13/orm/session_transaction.html
-        test_db_settings = database_credentials.test_db_settings()
-        self.engine = create_engine(test_db_settings['TEST_DB_CONNECTION_STRING'])
-        # connect to the database
-        self.connection = self.engine.connect()
-
-        # begin a non-ORM transaction
-        self.trans = self.connection.begin()
-
-        # bind an individual Session to the connection
-        s = sessionmaker()
-        self.session = s(bind=self.connection)
-
-        self.Base = automap_base()
-        self.Base.prepare(self.engine, reflect=True)
+        pass
 
     def setup_rows(self, session, base_table_class, rows):
         for row_dict in rows:
-            record = self.table_base_class(**row_dict)
-            self.session.add(record)
-            self.session.commit()
+            record = base_table_class(**row_dict)
+            session.add(record)
 
     def append_job_info(self, row_dict, processing_cycle_field_name, processing_cycle_id):
         job_info = {
@@ -45,71 +28,63 @@ class TestSqlAlchemyReader(unittest.TestCase):
         row_dict.update(job_info)
 
     def test_read_empty_table(self):
-        self.table_base_class = self.Base.classes.dw_stg_1_mai01_z00
-        reader = SqlAlchemyReader(self.session, self.table_base_class, 'em_create_dw_prcsng_cycle_id', 9999)
+        with dwetl.test_database_session() as session:
+            table_base_class = dwetl.Base.classes.dw_stg_1_mai01_z00
+            reader = SqlAlchemyReader(session, table_base_class, 'em_create_dw_prcsng_cycle_id', 9999)
 
-        results = []
-        for row in reader:
-            results.append(row)
+            results = []
+            for row in reader:
+                results.append(row)
 
-        self.assertEqual(0, len(results))
+            self.assertEqual(0, len(results))
 
     def test_read_rows_from_table(self):
-        # Add some sample rows to the "dw_stg_1_mai01_z00" table
-        self.table_base_class = self.Base.classes.dw_stg_1_mai01_z00
+        with dwetl.test_database_session() as session:
+            # Add some sample rows to the "dw_stg_1_mai01_z00" table
+            table_base_class = dwetl.Base.classes.dw_stg_1_mai01_z00
 
-        rows = [
-            {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000205993',
-             'z00_doc_number': '000205993', 'z00_no_lines': '0043', 'z00_data_len': '001583'},
-            {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000245526'}
-        ]
+            rows = [
+                {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000205993',
+                 'z00_doc_number': '000205993', 'z00_no_lines': '0043', 'z00_data_len': '001583'},
+                {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000245526'}
+            ]
 
-        for row in rows:
-            self.append_job_info(row, 'em_create_dw_prcsng_cycle_id', 9999)
+            for row in rows:
+                self.append_job_info(row, 'em_create_dw_prcsng_cycle_id', 9999)
 
-        self.setup_rows(self.session, self.table_base_class, rows)
+            self.setup_rows(session, table_base_class, rows)
 
-        reader = SqlAlchemyReader(self.session, self.table_base_class, 'em_create_dw_prcsng_cycle_id', 9999)
-        results = []
-        for row in reader:
-            results.append(row)
+            reader = SqlAlchemyReader(session, table_base_class, 'em_create_dw_prcsng_cycle_id', 9999)
+            results = []
+            for row in reader:
+                results.append(row)
 
-        self.assertEqual(2, len(results))
-        self.assertEqual('000205993', results[0]['rec_trigger_key'])
-        self.assertEqual('000245526', results[1]['rec_trigger_key'])
+            self.assertEqual(2, len(results))
+            self.assertEqual('000205993', results[0]['rec_trigger_key'])
+            self.assertEqual('000245526', results[1]['rec_trigger_key'])
 
     def test_read_rows_from_table_with_multiple_processing_ids(self):
-        # Add some sample rows to the "dw_stg_1_mai01_z00" table
-        self.table_base_class = self.Base.classes.dw_stg_1_mai01_z00
+        with dwetl.test_database_session() as session:
+            # Add some sample rows to the "dw_stg_1_mai01_z00" table
+            table_base_class = dwetl.Base.classes.dw_stg_1_mai01_z00
 
-        rows = [
-            {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000205993',
-             'z00_doc_number': '000205993', 'z00_no_lines': '0043', 'z00_data_len': '001583'},
-            {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000245526'}
-        ]
+            rows = [
+                {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000205993',
+                 'z00_doc_number': '000205993', 'z00_no_lines': '0043', 'z00_data_len': '001583'},
+                {'rec_type_cd': 'D', 'db_operation_cd': 'U', 'rec_trigger_key': '000245526'}
+            ]
 
-        # Use different em_create_dw_prcsng_cycle_ids for each row
-        self.append_job_info(rows[0], 'em_create_dw_prcsng_cycle_id', 1234)
-        self.append_job_info(rows[1], 'em_create_dw_prcsng_cycle_id', 9876)
+            # Use different em_create_dw_prcsng_cycle_ids for each row
+            self.append_job_info(rows[0], 'em_create_dw_prcsng_cycle_id', 1234)
+            self.append_job_info(rows[1], 'em_create_dw_prcsng_cycle_id', 9876)
 
-        self.setup_rows(self.session, self.table_base_class, rows)
+            self.setup_rows(session, table_base_class, rows)
 
-        reader = SqlAlchemyReader(self.session, self.table_base_class, 'em_create_dw_prcsng_cycle_id', 9876)
-        results = []
-        for row in reader:
-            results.append(row)
+            reader = SqlAlchemyReader(session, table_base_class, 'em_create_dw_prcsng_cycle_id', 9876)
+            results = []
+            for row in reader:
+                results.append(row)
 
-        # Only the row matching the em_create_dw_prcsng_cycle_id should be returned
-        self.assertEqual(1, len(results))
-        self.assertEqual('000245526', results[0]['rec_trigger_key'])
-
-    def tearDown(self):
-        self.session.close()
-
-        # rollback - everything that happened with the
-        # Session above (including calls to commit())
-        # is rolled back.
-        self.trans.rollback()
-
-        # return connection to the Engine
-        self.connection.close()
+            # Only the row matching the em_create_dw_prcsng_cycle_id should be returned
+            self.assertEqual(1, len(results))
+            self.assertEqual('000245526', results[0]['rec_trigger_key'])
