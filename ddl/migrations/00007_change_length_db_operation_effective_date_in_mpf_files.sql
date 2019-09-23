c12f6dd33fce3b89f404836d4aa0d6eae25e06e3
Tiffany Schoneboom <thschone@umd.edu>
2019-09-23 12:13:13 -0400

    LIBILS-370. Change db_operation_effective_date length in mpf files.
    Saw that length was incorrect for effective_date. Changed to 10.
    
    https://issues.umd.edu/browse/LIBILS-370

---
+++ ddl/migrations/00007_change_length_db_operation_effective_date_in_mpf_files.sql
@@ -0,0 +1,17 @@
+ALTER TABLE dw_stg_1_mpf_collection
+ALTER COLUMN db_operation_effective_date TYPE char(10);
+
+ALTER TABLE dw_stg_1_mpf_item_prcs_status
+ALTER COLUMN db_operation_effective_date TYPE char(10);
+
+ALTER TABLE dw_stg_1_mpf_item_status
+ALTER COLUMN db_operation_effective_date TYPE char(10);
+
+ALTER TABLE dw_stg_1_mpf_lbry_entity
+ALTER COLUMN db_operation_effective_date TYPE char(10);
+
+ALTER TABLE dw_stg_1_mpf_matrl_form
+ALTER COLUMN db_operation_effective_date TYPE char(10);
+
+ALTER TABLE dw_stg_1_mpf_mbr_lbry
+ALTER COLUMN db_operation_effective_date TYPE char(10);
