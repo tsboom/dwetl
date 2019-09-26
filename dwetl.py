7fdb511486434de9e634fdc3bc4cd97bf2adf0e8
Tiffany Schoneboom <thschone@umd.edu>
2019-09-26 15:47:57 -0400

    LIBILS-370. Create run function in dwetl file so it can have arguments.
    dwetl.py can be run by the command line and take data directory as an
    argument.
    
    https://issues.umd.edu/browse/LIBILS-370

--- dwetl.py
+++ dwetl.py
@@ -8,14 +8,12 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy import MetaData
 import pdb
 import pprint
-from colorama import init, Fore, Back, Style
-init()
 import re
 import csv
 import json
 import sql
 import os
-from os import walk
+import sys
 import logging
 import datetime
 import sqlalchemy
@@ -23,53 +21,44 @@ import dwetl.database_credentials as database_credentials
 from dwetl.job_info import JobInfoFactory, JobInfo
 import load_stage_1
 
+def run(input_directory):
+    time_started = datetime.datetime.now()
 
-time_started = datetime.datetime.now()
+    '''
+    create job_info for current process
+    '''
+    with dwetl.database_session() as session:
+        job_info_table_class = dwetl.Base.classes['dw_prcsng_cycle']
+        job_info = JobInfoFactory.create_job_info_from_db(session, job_info_table_class)
+
+    '''
+    load_stage_1
+    '''
+    load_stage_1.load_stage_1(job_info, input_directory)
+
+    '''
+    load 'in_' values from stg1 to stg 2 tables
+    '''
+    endtime = datetime.datetime.now()
+    elapsed_time = endtime - time_started
+    print("elapsed time: ", str(elapsed_time))
+
 
 '''
-get today's date YYYYMMD to set input directory
+main function for running script from the command line
 '''
+if __name__=='__main__':
+    arguments = sys.argv
 
-today = datetime.datetime.now().strftime('%Y%m%d')
-input_directory = f'data/{today}/'
+    today = datetime.datetime.now().strftime('%Y%m%d')
+    input_directory = f'data/{today}/'
 
-'''
-create job_info for current process
-'''
-with dwetl.database_session() as session:
-    job_info_table_class = dwetl.Base.classes['dw_prcsng_cycle']
-    job_info = JobInfoFactory.create_job_info_from_db(session, job_info_table_class)
+    # give hint if --help
+    if '--help' in arguments:
+        print('Usage: ')
+        print('\tdwetl.py [data directory]')
+        sys.exit(1)
+    if len(arguments) == 2:
+        input_directory = arguments[1]
 
-
-'''
-load_stage_1
-'''
-load_stage_1.load_stage_1(job_info, input_directory)
-
-
-'''
-load 'in_' values from stg1 to stg 2 tables
-'''
-
-endtime = datetime.datetime.now()
-elapsed_time = endtime - time_started
-print("elapsed time: ", str(elapsed_time))
-
-# def setup_logger(name, log_file, level=logging.DEBUG):
-#     # logging format
-#     formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-#     # set up logging file
-#     handler = logging.FileHandler(log_file)
-#     handler.setFormatter(formatter)
-#     logger = logging.getLogger(name)
-#     logger.setLevel(level)
-#     logger.addHandler(handler)
-#     return logger
-#
-# # dwetl logger
-# # log file with today's date
-# logfile = datetime.datetime.now().strftime('dwetl.log.%Y%m%d')
-# dwetl_logger = setup_logger('dwetl_logger', 'logs/'+ logfile)
-# # sqlalchemy logger
-# sa_logfile = datetime.datetime.now().strftime('sqlalchemy.engine.log.%Y%m%d')
-# sa_engine_logger = setup_logger('sqlalchemy.engine', 'logs/'+ sa_logfile)
+    run(input_directory)
