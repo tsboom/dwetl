0a649df83f8192cdb13ce9ba57e3c64b140e0aea
Tiffany Schoneboom <thschone@umd.edu>
2019-10-01 00:20:50 -0400

    LIBILS-267. Add load stage 2.py to dwetl.py
    Update table mapping for stage 2.
    Use load_stage_2.py in dwetl.py.
    
    https://issues.umd.edu/browse/LIBILS-267

--- load_stage_2.py
+++ load_stage_2.py
@@ -21,8 +21,26 @@ def aleph_library(table_name):
 
 def load_stage_2(job_info):
     stage1_to_stage2_table_mappings = {
-        'dw_stg_1_mai01_z00': 'dw_stg_2_bib_rec_z00',
-        'dw_stg_1_mai50_z30': 'dw_stg_2_lbry_item_z30'
+        "dw_stg_1_mai39_z13": "dw_stg_2_bib_rec_z13",
+        'dw_stg_1_mai01_z13': "dw_stg_2_bib_rec_z13",
+        "dw_stg_1_mai01_z13u": "dw_stg_2_bib_rec_z13u",
+        "dw_stg_1_mai39_z00": "dw_stg_2_bib_rec_z00",
+        "dw_stg_1_mai39_z13": "dw_stg_2_bib_rec_z13",
+        "dw_stg_1_mai39_z13u": "dw_stg_2_bib_rec_z13u",
+        "dw_stg_1_mai60_z00": "dw_stg_2_lbry_holding_z00",
+        "dw_stg_1_mai60_z13": "dw_stg_2_lbry_holding_z13",
+        "dw_stg_1_mai60_z13u": "dw_stg_2_lbry_holding_z13",
+        "dw_stg_1_mai50_z30": "dw_stg_2_lbry_item_z30",
+        "dw_stg_1_mai50_z35": "dw_stg_2_lbry_item_event_z35",
+        "dw_stg_1_mai01_z00_field": "dw_stg_2_bib_rec_z00_field",
+        "dw_stg_1_mai39_z00_field": "dw_stg_2_bib_rec_z00_field",
+        "dw_stg_1_mai60_z00_field": "dw_stg_2_lbry_holding_z00_field",
+        "dw_stg_1_mpf_mbr_lbry": "dw_stg2_mbr_lbry",
+        "dw_stg_1_mpf_lbry_entity": "dw_stg2_lbry_entity",
+        "dw_stg_1_mpf_collection": "dw_stg_2_collection",
+        "dw_stg_1_mpf_item_status": "dw_stg_2_item_status",
+        "dw_stg_1_mpf_item_prcs_status": "dw_stg_2_item_prcs_status",
+        "dw_stg_1_mpf_matrl_form": "dw_stg_2_matrl_form"
     }
 
     logger = None
