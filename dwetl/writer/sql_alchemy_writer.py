e1893cb2b7373bbb5e5a16b4da4f200c66f904a8
Tiffany Schoneboom <thschone@umd.edu>
2019-10-03 13:09:44 -0400

    LIBILS-267. Changed SA writer class to write k, v pairs for existing keys.
    We kept getting errors about keys from stage 1 not existing in stage 2,
    so we modified sql_alchemy_writer.py so that keys are matched with the
    table before being added to the row_dict.
    
    https://issues.umd.edu/browse/LIBILS-267

--- dwetl/writer/sql_alchemy_writer.py
+++ dwetl/writer/sql_alchemy_writer.py
@@ -1,5 +1,6 @@
 from sqlalchemy import exc
 from dwetl.writer.writer import Writer
+import pdb
 
 
 class SqlAlchemyWriter(Writer):
@@ -20,7 +21,12 @@ class SqlAlchemyWriter(Writer):
             # TODO: check if row_dict keys are in the db, only put matching keys and
             # values into the record
             matching_row_dict = {}
-            # insert the row into SQLAlchemy table base class
+            # find a list of keys from the table.
+            table_keys = self.table_base_class.__table__.columns.keys()
+            for key, val in row_dict.items():
+                if key in table_keys:
+                    matching_row_dict[key] = val
+            # insert the matching keys row into SQLAlchemy table base class
             record = self.table_base_class(**matching_row_dict)
             self.session.add(record)
         except exc.SQLAlchemyError as e:
