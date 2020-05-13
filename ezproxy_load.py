dca2eda4c2d5cdde47099ed6a75281720d63b239
Tiffany Schoneboom <thschone@umd.edu>
2020-05-13 12:18:32 -0400

    LIBILS-469. loading stage 2 (in progress).

--- ezproxy_load.py
+++ ezproxy_load.py
@@ -7,6 +7,8 @@ from dwetl.job_info import JobInfoFactory, JobInfo
 from dwetl.reader.ezproxy_reader import EzproxyReader
 from dwetl.processor.load_aleph_tsv import LoadAlephTsv
 from dwetl.writer.sql_alchemy_writer import SqlAlchemyWriter
+from dwetl.reader.sql_alchemy_reader import SqlAlchemyReader
+from dwetl.processor.copy_stage1_to_stage2 import CopyStage1ToStage2
 
 '''
 load EZ Proxy file equivalent table (Stage 1)
@@ -27,6 +29,26 @@ def load_stage_1(job_info, input_file, logger):
         processor.execute()
 
 
+
+def load_stage_2(job_info, logger):
+    print('EZProxy Loading stage 2...')
+    logger.info('EZProxy Loading stage 2...')
+    
+    processing_cycle_id = job_info.prcsng_cycle_id
+    
+    with dwetl.database_session() as session:
+        stage1_table_class = dwetl.Base.classes["dw_stg_1_ezp_sessns_snap"]
+        stage2_table_class = dwetl.Base.classes["dw_stg_2_ezp_sessns_snap"]
+        reader = SqlAlchemyReader(session, stage1_table_class, 'em_create_dw_prcsng_cycle_id', processing_cycle_id)
+        writer = SqlAlchemyWriter(session, stage2_table_class)
+        # there is no aleph library for ez proxy data, but CopyStage1ToStage2 still will work
+        library = ''
+        processor = CopyStage1ToStage2.create(reader, writer, job_info, logger, library)
+        processor.execute()
+    
+# def intertable_processing(job_info, logger):
+    # stuff
+    
 # '''
 # TODO: be able to load stage 1 from the command line using this one script
 # main function for running script from the command line
