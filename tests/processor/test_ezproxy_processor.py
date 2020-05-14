a27484960871bb6a3344ad1d6233986de58965e6
Tiffany Schoneboom <thschone@umd.edu>
2020-05-14 11:10:55 -0400

    LIBILS-469. test ezproxy processor (in progress)

---
+++ tests/processor/test_ezproxy_processor.py
@@ -0,0 +1,42 @@
+import unittest
+import datetime
+from dwetl.reader.list_reader import ListReader
+from dwetl.writer.list_writer import ListWriter
+from dwetl.job_info import JobInfo
+from dwetl.processor.ezproxy_processor import EzproxyProcessor
+from tests import test_logger
+import logging
+import pdb
+
+
+class TestEzproxyProcessor(unittest.TestCase):
+    maxDif= None
+    @classmethod
+    def setUpClass(cls):
+        cls.logger = test_logger.logger
+        
+        cls.sample_data = [{'em_create_dw_job_exectn_id': 1,
+                        'em_create_dw_job_name': 'CopyStage1ToStage2',
+                        'em_create_dw_job_version_no': '1.0.0',
+                        'em_create_dw_prcsng_cycle_id': 1,
+                        'em_create_tmstmp': datetime.datetime(2020, 5, 13, 15, 40, 10, 575382),
+                        'em_create_user_id': 'thschone',
+                        'in_ezp_sessns_snap_actv_sessns_cnt': 20,
+                        'in_ezp_sessns_snap_tmstmp': '20200509-0000',
+                        'in_ezp_sessns_virtual_hosts_cnt': 2718,
+                        'in_mbr_lbry_cd': 'ub'}]
+        
+    def test_convert_timestemp(self):
+        
+        result = EzproxyProcessor.convert_timestamp(self.sample_data[0])
+        
+        expected_result =  datetime.strptime('20200509-0000', '%Y%m%d-%H%M')
+        
+        self.assertEqual(expected_result, result)
+        
+    def test_library_dim_lookup(self):
+        result = EzproxyProcessor.library_dim_lookup(self.sample_data[0])
+        expected_result = 10
+        
+        self.assertEqual(expected_result, result)
+        
