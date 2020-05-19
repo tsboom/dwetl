21e7d6c5bd1053647d3ec2b89c1a955dbb056234
Tiffany Schoneboom <thschone@umd.edu>
2020-05-19 11:19:14 -0400

    LIBILS-469. added time of day dimension lookup.

--- ddl/usmai_dw_etl.sql
+++ ddl/usmai_dw_etl.sql
@@ -1297,6 +1297,7 @@ CREATE TABLE public.dw_stg_2_ezp_sessns_snap (
     in_ezp_sessns_snap_tmstmp character varying(13) NOT NULL,
     t1_ezp_sessns_snap_tmstmp__ezp_sessns_snap_clndr_dt_dim_key bigint,
     t2_ezp_sessns_snap_tmstmp__ezp_sessns_snap_tmstmp timestamp without time zone,
+    t3_ezp_sessns_snap_tmstmp__ezp_sessns_snap_time_of_day_dim_key integer,
     in_ezp_sessns_snap_actv_sessns_cnt integer,
     t1_ezp_sessns_snap_actv_sessns_cnt integer,
     in_ezp_sessns_virtual_hosts_cnt integer,
--- dwetl/processor/ezproxy_processor.py
+++ dwetl/processor/ezproxy_processor.py
@@ -16,29 +16,7 @@ class EzproxyProcessor(Processor):
 
     def job_name(self):
         return 'EzproxyProcessor'
-
-    @classmethod
-    def library_dim_lookup(cls, item, timestamp):
-        """
-        using the mbr_lbry_cd, find the mbr_lbry_dim_key and put into 
-        t1_mbr_lbry_cd__ezp_sessns_snap_mbr_lbry_dim_key
-        """
-        library_code = item['in_mbr_lbry_cd'].upper()
         
-        
-        with dwetl.reporting_database_session() as session2:
-            MemberLibrary = dwetl.ReportingBase.classes.dim_mbr_lbry
-            
-            # look up the mbr_lbry_dim_key 
-            matching_row = session2.query(MemberLibrary).\
-                filter(MemberLibrary.mbr_lbry_cd==library_code).\
-                filter(func.date(MemberLibrary.rm_rec_effective_from_dt)<timestamp).\
-                filter(func.date(MemberLibrary.rm_rec_effective_to_dt)>timestamp).first()
-
-            mbr_lbry_dim_key = matching_row.mbr_lbry_dim_key
-
-        return mbr_lbry_dim_key
-
     @classmethod
     def clndr_dt_dim_lookup(cls, item):
         """
@@ -54,13 +32,68 @@ class EzproxyProcessor(Processor):
         with dwetl.reporting_database_session() as session2:
             
             dim_date = dwetl.ReportingBase.classes.dim_date
-            # look up the mbr_lbry_dim_key 
-            matching_row = session2.query(dim_date).filter_by(clndr_dt=datestring).first()
+            # look up the calendar date
+            matching_row = session2.query(dim_date).\
+                filter(dim_date.clndr_dt==datestring).\
+                filter(date >= func.date(dim_date.rm_rec_effective_from_dt)).\
+                filter(date < func.date(dim_date.rm_rec_effective_to_dt)).first()
             
             clndr_dt_dim_key = matching_row.clndr_dt_dim_key
-            
-        return clndr_dt_dim_key
         
+        return clndr_dt_dim_key
+
+        
+    @classmethod
+    def time_of_day_dim_key_lookup(cls, item):
+        """
+        using ezp_sessns_snap_tmstmp
+        get ezp_sessns_snap_time_of_day_dim_key
+        """
+        
+        timestamp = item['in_ezp_sessns_snap_tmstmp']
+        
+        date = datetime.strptime(timestamp, '%Y%m%d-%H%M')
+        
+        # need a string HH:MM:SS
+        timestring = date.strftime('%H:%M:%S')
+        
+        with dwetl.reporting_database_session() as session2:
+            dim_time_of_day = dwetl.ReportingBase.classes.dim_time_of_day
+            
+            #look up the time of day 
+            matching_row = session2.query(dim_time_of_day).\
+                filter(dim_time_of_day.time_of_day==timestring).\
+                filter(date >= func.date(dim_time_of_day.rm_rec_effective_from_dt)).\
+                filter(date < func.date(dim_time_of_day.rm_rec_effective_to_dt)).first()
+            
+            time_dim_key = matching_row.time_of_day_dim_key  
+        return time_dim_key
+            
+    @classmethod
+    def library_dim_lookup(cls, item):
+        """
+        using the mbr_lbry_cd, find the mbr_lbry_dim_key and put into 
+        t1_mbr_lbry_cd__ezp_sessns_snap_mbr_lbry_dim_key
+        """
+        library_code = item['in_mbr_lbry_cd'].upper()
+        
+        timestamp = item['in_ezp_sessns_snap_tmstmp']
+        
+        date = datetime.strptime(timestamp, '%Y%m%d-%H%M')
+        
+        
+        with dwetl.reporting_database_session() as session2:
+            MemberLibrary = dwetl.ReportingBase.classes.dim_mbr_lbry
+            
+            # look up the mbr_lbry_dim_key 
+            matching_row = session2.query(MemberLibrary).\
+                filter(MemberLibrary.mbr_lbry_cd==library_code).\
+                filter(date >= func.date(MemberLibrary.rm_rec_effective_from_dt)).\
+                filter(date < func.date(MemberLibrary.rm_rec_effective_to_dt)).first()
+
+            mbr_lbry_dim_key = matching_row.mbr_lbry_dim_key
+
+        return mbr_lbry_dim_key
         
     @classmethod
     def convert_timestamp(cls, item):
@@ -91,17 +124,22 @@ class EzproxyProcessor(Processor):
                 elif key == "in_ezp_sessns_snap_tmstmp":
                     # save value in pk
                     out_dict[key] = value
-                
+                    
                     timestamp = EzproxyProcessor.convert_timestamp(item)
                 
                     out_dict['t2_ezp_sessns_snap_tmstmp__ezp_sessns_snap_tmstmp'] = timestamp
-
+                    
+                    # look up calendar_date_dim_key
                     calendar_date_dim_key = EzproxyProcessor.clndr_dt_dim_lookup(item)
                     out_dict['t1_ezp_sessns_snap_tmstmp__ezp_sessns_snap_clndr_dt_dim_key'] = calendar_date_dim_key
+                    
+                    # look up time of day dim key 
+                    time_of_day_dim_key = EzproxyProcessor.time_of_day_dim_key_lookup(item)
+                    out_dict['t3_ezp_sessns_snap_tmstmp__ezp_sessns_snap_time_of_day_dim_key'] = time_of_day_dim_key
 
                 elif key == "in_mbr_lbry_cd":
                     out_dict[key] = value
-                    library_dim_key = EzproxyProcessor.library_dim_lookup(item, timestamp)
+                    library_dim_key = EzproxyProcessor.library_dim_lookup(item)
                     out_dict['t1_mbr_lbry_cd__ezp_sessns_snap_mbr_lbry_dim_key'] = library_dim_key
                 else:
                     target_col_name = key.replace('in_', 't1_')
