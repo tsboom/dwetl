bf65623ecd41e5d3ce43ac1090a4cb554165072b
Tiffany Schoneboom <thschone@umd.edu>
2021-09-24 14:05:55 -0400

    update tasks.py so that usmai_dw_etl_test.sql is used to reset test database

--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 from invoke import task, exceptions
 import dwetl.database_credentials
+import pdb
 
 @task
 def sheets_to_json(c):
@@ -15,10 +16,11 @@ def database_reset(c):
     """
     db_settings = dwetl.database_credentials.db_settings()
     db_user = db_settings['DB_USER']
+    db_name = db_settings['DB_NAME']
     db_host = db_settings['DB_HOST_NAME']
     db_port = db_settings['DB_PORT']
     db_password = db_settings['DB_PASSWORD']
-    reset_database(c, db_host, db_port, db_user, db_password)
+    reset_database(c, db_host, db_name, db_port, db_user, db_password)
 
 @task
 def test_database_reset(c):
@@ -30,10 +32,11 @@ def test_database_reset(c):
     """
     test_db_settings = dwetl.database_credentials.test_db_settings()
     test_db_user = test_db_settings['TEST_DB_USER']
+    test_db_name = test_db_settings['TEST_DB_NAME']
     test_db_host = test_db_settings['TEST_DB_HOST_NAME']
     test_db_port = test_db_settings['TEST_DB_PORT']
     test_db_password = test_db_settings['TEST_DB_PASSWORD']
-    reset_database(c, test_db_host, test_db_port, test_db_user, test_db_password)
+    reset_database(c, test_db_host, test_db_name, test_db_port, test_db_user, test_db_password)
 
 @task
 def run_migration(c, sql_file):
@@ -78,23 +81,23 @@ def update_db_ddl(c):
         print('An error has occurred')
 
 # Task helper function
-def reset_database(context, db_host, db_port, db_user, db_password):
+def reset_database(context, db_host, db_name, db_port, db_user, db_password):
     pg_password=f'PGPASSWORD={db_password} '
     psql_cmd = f'psql -U {db_user} -d postgres --host={db_host} --port={db_port}'
 
     ask_for_confirmation = True
     if (db_host == 'localhost' or db_host == '127.0.0.1') and db_port == '5432':
         # Assume localhost:5432 and 127.0.0.1:5432 are local Postgres
-        ask_for_confirmation = False
+        ask_for_confirmation = True
 
     if ask_for_confirmation:
-        confirm = input(f"Are you sure you want to reset {db_host}:{db_port}? (y/n) ")
+        confirm = input(f"Are you sure you want to reset {db_name} {db_host}:{db_port}? (y/n) ")
         confirm = confirm.lower()
         if not((confirm == 'y') or (confirm == 'yes')):
             print("Database has not been reset.")
             exit()
 
-    print(f'Resetting database at {db_host}:{db_port}')
+    print(f'Resetting {db_name} database at {db_host}:{db_port}')
     print('-----------')
     print('Terminating sessions and dropping database')
     terminate_sessions = psql_cmd + ' -f ddl/drop_db.sql'
@@ -106,9 +109,16 @@ def reset_database(context, db_host, db_port, db_user, db_password):
     else:
         raise Exception()
         print('An error has occurred')
-
-    print('Creating database from DDL')
-    load_ddl = psql_cmd + ' -f ddl/usmai_dw_etl.sql'
+    
+    
+    # If resetting the test dabase,
+    # create a test db DDL from the usmai_dw_etl.sql to keep it up to date. 
+    if db_name == 'usmai_dw_etl_test':
+        print('Creating database from usmai_dw_etl_test.sql DDL')
+        load_ddl = psql_cmd + ' -f ddl/usmai_dw_etl_test.sql'
+    else: 
+        load_ddl = psql_cmd + ' -f ddl/usmai_dw_etl.sql'
+        
     print(load_ddl)
     if context.run(pg_password + load_ddl):
         print('-----------')
