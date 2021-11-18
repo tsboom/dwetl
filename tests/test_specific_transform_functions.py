b1c5f2ed225c6575ea5bfe10cb22b8d0b70e5c34
Nima Asadi <nasadi1@librltsm250212.umd.edu>
2021-11-18 15:43:20 -0500

    LIBILS-434. tests for lookup tables added.

--- tests/test_specific_transform_functions.py
+++ tests/test_specific_transform_functions.py
@@ -13,7 +13,7 @@ class TestSpecificTransformFunctions(unittest.TestCase):
     #     #test isbn_issn check function
     #     field = TransformField('z13_isbn_issn','177091921X','020')
     #     self.assertEqual(specific_transform_functions.isbn_code_020(field),'177091921X')
-    # 
+    #
     #     field = TransformField('z13_isbn_issn','177091921X','022')
     #     self.assertEqual(specific_transform_functions.issn_code_022(field),'177091921X')
     # def test_remove_ocm_ocn_on(self):
@@ -26,11 +26,43 @@ class TestSpecificTransformFunctions(unittest.TestCase):
         self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nam^^2200241d1^45^0'),'Language material')
         self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^ncm^^2200241d1^45^0'),'Notated music')
         self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^ndm^^2200241d1^45^0'),'Manuscript notated music')
-        
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nem^^2200241d1^45^0'),'Cartographic material')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nfm^^2200241d1^45^0'),'Manuscript cartographic material')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^ngm^^2200241d1^45^0'),'Projected medium')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nim^^2200241d1^45^0'),'Nonmusical sound recording')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^njm^^2200241d1^45^0'),'Musical sound recording')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nkm^^2200241d1^45^0'),'Two-dimensional nonprojectable graphic')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nmm^^2200241d1^45^0'),'Computer file')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nom^^2200241d1^45^0'),'Kit')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^npm^^2200241d1^45^0'),'Mixed materials')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^nrm^^2200241d1^45^0'),'Three-dimensional artifact or naturally occurring object')
+        self.assertEqual(specific_transform_functions.lookup_record_type('^^^^^ntm^^2200241d1^45^0'),'Manuscript language material')
+    #testing lookup_tables/bibliographic_level.csv for bib rec z13u
     def test_lookup_bibliographic_level(self):
         self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nam^^2200241d1^45^0'),'Monograph/Item')
+        self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^naa^^2200241d1^45^0'),'Monographic component part')
         self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nab^^2200241d1^45^0'),'Serial component part')
         self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nac^^2200241d1^45^0'),'Collection')
+        self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nad^^2200241d1^45^0'),'Subunit')
+        self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nai^^2200241d1^45^0'),'Integrating resource')
+        self.assertEqual(specific_transform_functions.lookup_bibliographic_level('^^^^^nas^^2200241d1^45^0'),'Serial')
+    #testing lookup_tables/encoding_level.csv for bib rec z13u
+    def test_lookup_encoding_level(self):
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241^1^45^0'),'Full-level by authorized national bibliographic agencies and libraries participating in PCC (BIBCO and CONSER).')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024111^45^0'),'Full-level material not examined.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024121^45^0'),'Less-than-full level material not examined.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024131^45^0'),'Abbreviated level.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024141^45^0'),'Core-level.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024151^45^0'),'Partial (preliminary) level.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024171^45^0'),'Minimal-level.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^220024181^45^0'),'Prepublication level.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241I1^45^0'),'Full-level input by OCLC participants.' )
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241K1^45^0'),'Minimal-level input by OCLC participants.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241L1^45^0'),'Added from a batch process.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241M1^45^0'),'Added from a batch process.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241J1^45^0'),'Deleted record.')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241u1^45^0'),'Unknown')
+        self.assertEqual(specific_transform_functions.lookup_encoding_level('^^^^^nam^^2200241z1^45^0'),'Not applicable')                                                                                                        
     # def test_is_suppressed(self):
     #     #test to confirm is_suppressed works with upper, lower, mixed, and fail
     #     field = TransformField('LBRY_HOLDING_DISPLAY_SUPPRESSED_FLAG', 'suppressed')
@@ -50,7 +82,7 @@ class TestSpecificTransformFunctions(unittest.TestCase):
     #     self.assertEqual(specific_transform_functions.is_acq_created(field),'Y')
     #     field = TransformField('z13u_user_defined_6','Shpoomples')
     #     self.assertEqual(specific_transform_functions.is_acq_created(field),'N')
-    # 
+    #
     # def test_is_circ_created(self):
     #     #test to confirm is_circ_created works with upper, lower, mixed, and fail
     #     field = TransformField('z13u_user_defined_6','circ-created suppressed ||')
@@ -59,15 +91,15 @@ class TestSpecificTransformFunctions(unittest.TestCase):
     #     self.assertEqual(specific_transform_functions.is_circ_created(field),'Y')
     #     field = TransformField('z13u_user_defined_6','Shpoomples')
     #     self.assertEqual(specific_transform_functions.is_circ_created(field),'N')
-    # 
+    #
     # def test_is_provisional(self):
     #     #test to confirm is_provisional works with upper, lower, mixed, and fail
     #     field = TransformField('z13u_user_defined_6', 'provisional')
     #     self.assertEqual(specific_transform_functions.is_provisional(field), 'Y')
-    # 
+    #
     #     field = TransformField('z13u_user_defined_6', 'pRoViSiOnAl')
     #     self.assertEqual(specific_transform_functions.is_provisional(field), 'Y')
-    # 
+    #
     #     field = TransformField('z13u_user_defined_6', 'Shpoomples')
     #     self.assertEqual(specific_transform_functions.is_provisional(field), 'N')
 
