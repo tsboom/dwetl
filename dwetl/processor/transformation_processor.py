01a9dde0a102e5acdf1a42dcfc3d678bee821421
Nima Asadi <nasadi1@LIBRLTSM250212.local>
2021-12-03 17:16:12 -0500

    LIBILS-434. preserve create metadata

--- dwetl/processor/transformation_processor.py
+++ dwetl/processor/transformation_processor.py
@@ -49,19 +49,19 @@ class TransformationProcessor(Processor):
 
         # *special casee for bib_rec z13* capture isbn_issn_code for processing isbn_issn later
         isbn_issn_code = None
-        
-        
+        pprint.pprint(item)
+
         # transform keys and vals within current item
         for key, val in item.items():
-
             # skip invalid keys
             if key in invalid_keys:
                 continue
-
             # add the pks to the out_dict so the row can be inserted later
             if key in pk_list:
                 out_dict[key] = val
-
+            # preserve create meta data
+            if key.startswith('em_create'):
+                out_dict[key] = val
             # only process dq values to transform them. skip keys from invalid_keys and keys that aren't 'dq_'
             if not key.startswith('dq_'):
                 continue
@@ -73,7 +73,6 @@ class TransformationProcessor(Processor):
 
             # get list of transformations for current key
             transform_steps = TransformationProcessor.get_transformations_for_key(key, json_config)
-
             # transform
             if transform_steps:
                 for transformation in transform_steps:
@@ -84,14 +83,14 @@ class TransformationProcessor(Processor):
                     # get index of current transformation (for t1, t2, etc)
                     index = transform_steps.index(transformation)
                     transform_number = index + 1
-                    
+
                     # form the column name to write to for t1_source_column__target_column, t2...
                     target_column = transformation_info.target_col_name
                     source_column = transformation_info.source_col_name
 
                     transform_column_name = f"t{transform_number}_{source_column}__{target_column}"
-                    
-                    # *special case z13 issn code 
+
+                    # *special case z13 issn code
                     # TODO: what happens if the keys are not run in order isbn_issn_code -> isbn_issn
                     if key == 'dq_z13_isbn_issn_code':
                         # save the isbn issn code and go to the next key
@@ -100,10 +99,10 @@ class TransformationProcessor(Processor):
                     if key == 'dq_z13_isbn_issn':
                         if isbn_issn_code and transform_column_name=='t1_z13_isbn_issn__bib_rec_isbn_txt':
                             transform_result = specific_transform_functions.isbn_code_020(isbn_issn_code, val)
-    
+
                         if isbn_issn_code and transform_column_name=='t2_z13_isbn_issn__bib_rec_all_associated_issns_txt':
                             transform_result = specific_transform_functions.issn_code_022(isbn_issn_code, val)
-                    else: 
+                    else:
                         # run transformation
                         transform_result = transformation_info.transform(val)
 
