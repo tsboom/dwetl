018a4b7117f4c114dca1766a25d235d4ddef47d3
Tiffany Schoneboom <thschone@umd.edu>
2022-01-27 02:36:46 -0500

    LIBILS-434. Capitalized Completed in dwetl output

--- stage_2_intertable_processing.py
+++ stage_2_intertable_processing.py
@@ -70,7 +70,7 @@ def stage_2_intertable_processing(job_info, logger, stg_2_table_config_mapping, 
             logger.info("transforming...")
             transformation_processor = TransformationProcessor(reader, writer, job_info, logger, json_config, pk_list, error_writer)
             transformation_processor.execute()
-            print('transformations completed.')
+            print('Transformations Completed.')
             logger.info('transformations completed.')
 
 
