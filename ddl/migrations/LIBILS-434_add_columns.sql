dd0b2c1afe06fd0188237c1ca46a1c01ed56ebee
Tiffany Schoneboom <thschone@umd.edu>
2022-02-07 20:53:37 -0500

    LIBILS-434. update database migration file to drop z13u and create it again with the right columns

--- ddl/migrations/LIBILS-434_add_columns.sql
+++ ddl/migrations/LIBILS-434_add_columns.sql
@@ -1,24 +1,92 @@
 BEGIN;
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-RENAME COLUMN dq__z13u_user_defined_2 TO dq_z13u_user_defined_2;
+-- need to drop this table and recreate it because when we add columns onto it the order of the columns is inconvenient. 
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-ADD COLUMN t3_z13u_user_defined_3__bib_rec_type_desc character varying(500);
+DROP TABLE dw_stg_2_bib_rec_z13u;
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-DROP COLUMN t3_z13u_user_defined_3__bib_rec_bib_lvl_cd,
-ADD COLUMN t4_z13u_user_defined_3__bib_rec_bib_lvl_cd character(1);
+CREATE TABLE public.dw_stg_2_bib_rec_z13u (
+    db_operation_cd character(1) NOT NULL,
+    dw_stg_2_aleph_lbry_name character(5) NOT NULL,
+    in_z13u_rec_key character(9) NOT NULL,
+    in_z13u_user_defined_2 character varying(500),
+    pp_z13u_user_defined_2 character varying(500),
+    dq_z13u_user_defined_2 character varying(500),
+    t1_z13u_user_defined_2__bib_rec_oclc_no character varying(500),
+    in_z13u_user_defined_3 character varying(500),
+    pp_z13u_user_defined_3 character varying(500),
+    dq_z13u_user_defined_3 character varying(500),
+    t1_z13u_user_defined_3__bib_rec_marc_rec_leader_field_txt character varying(500),
+    t2_z13u_user_defined_3__bib_rec_type_cd character(1),
+    t3_z13u_user_defined_3__bib_rec_type_desc character varying(500),
+    t4_z13u_user_defined_3__bib_rec_bib_lvl_cd character(1),
+    t5_z13u_user_defined_3__bib_rec_bib_lvl_desc character varying(500),
+    t6_z13u_user_defined_3__bib_rec_encoding_lvl_cd character(1),
+    t7_z13u_user_defined_3__bib_rec_encoding_lvl_desc character varying(500),
+    in_z13u_user_defined_4 character varying(500),
+    pp_z13u_user_defined_4 character varying(500),
+    dq_z13u_user_defined_4 character varying(500),
+    t1_z13u_user_defined_4__bib_rec_marc_rec_008_field_txt character varying(500),
+    t2_z13u_user_defined_4__bib_rec_language_cd character(3),
+    in_z13u_user_defined_5 character varying(500),
+    pp_z13u_user_defined_5 character varying(500),
+    dq_z13u_user_defined_5 character varying(500),
+    t1_z13u_user_defined_5__bib_rec_issn character varying(500),
+    in_z13u_user_defined_6 character varying(500),
+    pp_z13u_user_defined_6 character varying(500),
+    dq_z13u_user_defined_6 character varying(500),
+    t1_z13u_user_defined_6__bib_rec_display_suppressed_flag character(1),
+    t2_z13u_user_defined_6__bib_rec_acquisition_created_flag character(1),
+    t3_z13u_user_defined_6__bib_rec_circulation_created_flag character(1),
+    t4_z13u_user_defined_6__bib_rec_provisional_status_flag character(1),
+    in_z13u_user_defined_1_code character(5),
+    in_z13u_user_defined_1 character varying(500),
+    in_z13u_user_defined_2_code character(5),
+    in_z13u_user_defined_3_code character(5),
+    in_z13u_user_defined_4_code character(5),
+    in_z13u_user_defined_5_code character(5),
+    in_z13u_user_defined_6_code character(5),
+    in_z13u_user_defined_7_code character(5),
+    in_z13u_user_defined_7 character varying(500),
+    in_z13u_user_defined_8_code character(5),
+    in_z13u_user_defined_8 character varying(500),
+    in_z13u_user_defined_9_code character(5),
+    in_z13u_user_defined_9 character varying(500),
+    in_z13u_user_defined_10_code character(5),
+    in_z13u_user_defined_10 character varying(500),
+    in_z13u_user_defined_11_code character(5),
+    in_z13u_user_defined_11 character varying(500),
+    in_z13u_user_defined_12_code character(5),
+    in_z13u_user_defined_12 character varying(500),
+    in_z13u_user_defined_13_code character(5),
+    in_z13u_user_defined_13 character varying(500),
+    in_z13u_user_defined_14_code character(5),
+    in_z13u_user_defined_14 character varying(500),
+    in_z13u_user_defined_15_code character(5),
+    in_z13u_user_defined_15 character varying(500),
+    rm_suspend_rec_flag character(1) DEFAULT 'N'::bpchar NOT NULL,
+    rm_suspend_rec_reason_cd character(3),
+    rm_dq_check_excptn_cnt smallint DEFAULT 0 NOT NULL,
+    em_create_dw_prcsng_cycle_id integer NOT NULL,
+    em_create_dw_job_exectn_id integer NOT NULL,
+    em_create_dw_job_name character varying(100) NOT NULL,
+    em_create_dw_job_version_no character varying(20) NOT NULL,
+    em_create_user_id character varying(20) NOT NULL,
+    em_create_tmstmp timestamp without time zone NOT NULL,
+    em_update_dw_prcsng_cycle_id integer,
+    em_update_dw_job_exectn_id integer,
+    em_update_dw_job_name character varying(100),
+    em_update_dw_job_version_no character varying(20),
+    em_update_user_id character varying(20),
+    em_update_tmstmp timestamp without time zone
+)
+WITH (autovacuum_enabled='true');
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-ADD COLUMN t5_z13u_user_defined_3__bib_rec_bib_lvl_desc character varying(500);
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-DROP COLUMN t4_z13u_user_defined_3__bib_rec_encoding_lvl_cd,
-ADD COLUMN t6_z13u_user_defined_3__bib_rec_encoding_lvl_cdc character(1);
+ALTER TABLE public.dw_stg_2_bib_rec_z13u OWNER TO usmai_dw;
 
-ALTER TABLE dw_stg_2_bib_rec_z13u
-ADD COLUMN t7_z13u_user_defined_3__bib_rec_encoding_lvl_desc character varying(500);
+ALTER TABLE ONLY public.dw_stg_2_bib_rec_z13u
+    ADD CONSTRAINT pk_dw_stg_2_bib_rec_z13u PRIMARY KEY (db_operation_cd, dw_stg_2_aleph_lbry_name, in_z13u_rec_key, em_create_dw_prcsng_cycle_id);
+
 
 ALTER TABLE dw_db_errors
 -- need to write test for this in end to end 
