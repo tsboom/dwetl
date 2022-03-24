fdcaae30d863d3a5bec4c5445644396bff86fadb
Tiffany Schoneboom <thschone@umd.edu>
2022-03-24 15:55:17 -0400

    LIBILS-634. change to mai60_z00_full_data

--- table_mappings.py
+++ table_mappings.py
@@ -5,13 +5,13 @@ table mapping for load_stage_1
 
 stg_1_table_mapping = {'ALEPH_TSV_TABLE_MAPPING': 
         {
-        "mai01_z00_data": "dw_stg_1_mai01_z00",
-        "mai39_z00_data": "dw_stg_1_mai39_z00",
-        "mai01_z13_data": "dw_stg_1_mai01_z13",
-        "mai39_z13_data": "dw_stg_1_mai39_z13",
-        "mai01_z13u_data": "dw_stg_1_mai01_z13u",
-        "mai39_z13u_data": "dw_stg_1_mai39_z13u",
-        "mai60_z00_data": "dw_stg_1_mai60_z00",
+        #"mai01_z00_data": "dw_stg_1_mai01_z00",
+        # "mai39_z00_data": "dw_stg_1_mai39_z00",
+        # "mai01_z13_data": "dw_stg_1_mai01_z13",
+        # "mai39_z13_data": "dw_stg_1_mai39_z13",
+        # "mai01_z13u_data": "dw_stg_1_mai01_z13u",
+        # "mai39_z13u_data": "dw_stg_1_mai39_z13u",
+        "mai60_z00_full_data": "dw_stg_1_mai60_z00",
         "mai60_z13_data": "dw_stg_1_mai60_z13",
         "mai60_z13u_data": "dw_stg_1_mai60_z13u",
         "mai60_z103_bib_data": "dw_stg_1_mai60_z103_bib",
