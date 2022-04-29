0f640c9a56b3da7782d38f154c5d58c64836542a
Tiffany Schoneboom <thschone@umd.edu>
2022-04-29 17:44:39 -0400

    LIBILS-635. fix run-test-migration tasks.py

--- tasks.py
+++ tasks.py
@@ -64,13 +64,13 @@ def run_test_migration(c, sql_file):
     """
     runs sql file for test db migration
     """
-    db_settings = dwetl.database_credentials.test_db_settings()
-    db_user = db_settings['TEST_DB_USER']
-    db_host = db_settings['TEST_DB_HOST_NAME']
-    db_port = db_settings['TEST_DB_PORT']
-    db_password = db_settings['TEST_DB_PASSWORD']
-    db_name = db_settings['TEST_DB_NAME']
-    pg_password=f'TEST_PGPASSWORD={db_password} '
+    test_db_settings = dwetl.database_credentials.test_db_settings()
+    test_db_user = test_db_settings['TEST_DB_USER']
+    test_db_name = test_db_settings['TEST_DB_NAME']
+    test_db_host = test_db_settings['TEST_DB_HOST_NAME']
+    test_db_port = test_db_settings['TEST_DB_PORT']
+    test_db_password = test_db_settings['TEST_DB_PASSWORD']
+    pg_password=f'PGPASSWORD={test_db_password} '
 
     psql_cmd = f'psql -U {db_user} -h {db_host} -p {db_port} -d {db_name} -f {sql_file}'
     if c.run(pg_password + psql_cmd):
