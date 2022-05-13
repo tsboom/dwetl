1efd6026bf19a7ce0b02e0c551600b1bd5d872a6
Nima Asadi <nasadi1@LIBRLTSM250212.local>
2022-05-13 17:02:20 -0400

    LIBILS-633. edited load stage 1 to add mpf files processing

--- load_stage_1.py
+++ load_stage_1.py
@@ -62,7 +62,7 @@ def load_stage_1(job_info, input_directory, logger, table_mapping, session_creat
     # load mpf tsv files
     # '''
     #
-    for file, table in MPF_TABLE_MAPPING.items():
+    for file, table in  table_mapping['MPF_TABLE_MAPPING'].items():
         file_path = os.path.join(input_directory, file)
         print(file_path)
         with dwetl.database_session() as session:
@@ -71,7 +71,7 @@ def load_stage_1(job_info, input_directory, logger, table_mapping, session_creat
             #writer = PrintWriter()
             writer = SqlAlchemyWriter(session, dwetl.Base.classes[table])
             logger = None
-            processor = LoadMpfTsv(reader, writer, job_info, logger)
+            processor = LoadMpfTsv(reader, writer, job_info, logger, error_writer)
 
             processor.execute()
 
--- table_mappings.py
+++ table_mappings.py
@@ -5,12 +5,13 @@ table mapping for load_stage_1
 
 stg_1_table_mapping = {'ALEPH_TSV_TABLE_MAPPING':
         {
-        "mai01_z00_data": "dw_stg_1_mai01_z00",
-        "mai39_z00_data": "dw_stg_1_mai39_z00",
-        "mai01_z13_data": "dw_stg_1_mai01_z13",
-        "mai39_z13_data": "dw_stg_1_mai39_z13",
-        "mai01_z13u_data": "dw_stg_1_mai01_z13u",
-        "mai39_z13u_data": "dw_stg_1_mai39_z13u"
+        # "mai01_z00_data": "dw_stg_1_mai01_z00",
+        # "mai39_z00_data": "dw_stg_1_mai39_z00",
+        # "mai01_z13_data": "dw_stg_1_mai01_z13",
+        # "mai39_z13_data": "dw_stg_1_mai39_z13",
+        # "mai01_z13u_data": "dw_stg_1_mai01_z13u",
+        # "mai39_z13u_data": "dw_stg_1_mai39_z13u"
+
         # "mai60_z00_data": "dw_stg_1_mai60_z00",
         # "mai60_z13_data": "dw_stg_1_mai60_z13",
         # "mai60_z13u_data": "dw_stg_1_mai60_z13u",
@@ -42,12 +43,13 @@ table mapping for load_stage_2
 '''
 
 stg_1_to_stg_2_table_mapping = {
-    "dw_stg_1_mai39_z13": "dw_stg_2_bib_rec_z13",
-    'dw_stg_1_mai01_z13': "dw_stg_2_bib_rec_z13",
-    "dw_stg_1_mai01_z13u": "dw_stg_2_bib_rec_z13u",
-    "dw_stg_1_mai01_z00": "dw_stg_2_bib_rec_z00",
-    "dw_stg_1_mai39_z00": "dw_stg_2_bib_rec_z00",
-    "dw_stg_1_mai39_z13u": "dw_stg_2_bib_rec_z13u",
+    # "dw_stg_1_mai39_z13": "dw_stg_2_bib_rec_z13",
+    # 'dw_stg_1_mai01_z13': "dw_stg_2_bib_rec_z13",
+    # "dw_stg_1_mai01_z13u": "dw_stg_2_bib_rec_z13u",
+    # "dw_stg_1_mai01_z00": "dw_stg_2_bib_rec_z00",
+    # "dw_stg_1_mai39_z00": "dw_stg_2_bib_rec_z00",
+    # "dw_stg_1_mai39_z13u": "dw_stg_2_bib_rec_z13u",
+
     # "dw_stg_1_mai60_z00": "dw_stg_2_lbry_holding_z00",
     # "dw_stg_1_mai60_z13": "dw_stg_2_lbry_holding_z13",
     # "dw_stg_1_mai60_z13u": "dw_stg_2_lbry_holding_z13u",
